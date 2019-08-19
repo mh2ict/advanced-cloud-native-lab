13fe65d1140de68d72615991c7fe9e05016b78d1
Munshi Hafizul Haque <mhaque@redhat.com>
2019-08-19 15:32:18 +0800

    first commit

---
+++ ocp_login.sh
@@ -0,0 +1,7 @@
+oc login -u user5 -p r3dh4t1! https://master.f4ca.openshift.opentlc.com
+export OCP_SERVER_URL=https://master.f4ca.openshift.opentlc.com
+export OCP_USER_ID=user5
+export COOLSTORE_PRJ=${OCP_USER_ID}-coolstore
+export JAVA_HOME=/usr/java/jdk1.8.0_221-amd64/ 
+export PATH=$JAVA_HOME/bin:$PATH
+
